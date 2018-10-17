a8f5ce6565ee49aa5965135c298910326e5f6c9b
gidraff <kamandegid@gmail.com>
2018-10-17 10:19:40 +0300

    chore(Setup Browserify): Configure Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,19 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); // Runs a local dev server
 var open = require('gulp-open'); // Open a url in a web
+var browserify = require('browserify'); // Bundles js
+var reactify = require('reactify'); // Transform React JSX to js
+var source = require('vinyl-source-stream'); // Use conventional text streams with gulp
+
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 }
 
@@ -36,10 +42,22 @@ gulp.task('html', function() {
         .pipe(connect.reload());
 });
 
+// Browserify Bundles all js into one file and put it in script folder under dist
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+})
+
 // watch files
 gulp.task('watch', function() {
     gulp.watch(config.paths.html, ['html']); // watched html path and if anything changes the html task will be run
+    gulp.watch(config.paths.js, ['js']);
 });
 
 // default gulp task
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
